4868178182b3882122dbfa6eddaef5f06cd9f914
Koshik Raj <koshik.raj@gmail.com>
2024-02-21 11:07:13 +0530

    Deploy website - based on 53910368ea5a6f62c4be6fd740078d2a96176a94

---
+++ assets/js/4edc808e.bab99094.js
@@ -0,0 +1,32 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[173],{4323:(e,t,a)=>{a.r(t),a.d(t,{assets:()=>m,contentTitle:()=>p,default:()=>b,frontMatter:()=>u,metadata:()=>c,toc:()=>h});var n=a(7462),o=a(7294),r=a(3905),s=a(508);s.ZP.button`
+	padding: 1.4rem;
+	min-width: 160px;
+	font-weight: 600;
+	font-size: 16px;
+	color: #fff;
+	// background: linear-gradient(132.56deg, #61FF47 -20.89%, #89B8FF 99.53%, #FF70F1 123.47%);
+	border-radius: 5px;
+	cursor: pointer;
+	border-width: 0px;
+`;const i=s.ZP.button`
+	padding: 1.4rem;
+	min-width: 160px;
+	font-weight: 600;
+	font-size: 16px;
+	color: #fff;
+	background: rgb(124, 197, 98);
+	border-radius: 5px;
+	cursor: pointer;
+	border-width: 0px;
+`,l=s.ZP.button`
+	padding: 1.4rem;
+	min-width: 160px;
+	font-weight: 600;
+	font-size: 16px;
+	color: #fff;
+	box-shadow: rgb(97, 255, 71) 0px 0px 2px;
+    background: rgb(32, 40, 61);
+	border-radius: 5px;
+	cursor: pointer;
+	border-width: 0px;
+`,d=e=>{let{children:t,className:a,...n}=e;return"green"==a?o.createElement(i,n,t):"outline"==a?o.createElement(l,n,t):o.createElement("div",null)},u={sidebar_position:1},p="Introduction",c={unversionedId:"index",id:"index",title:"Introduction",description:"Smarter Module Marketplace",source:"@site/docs/index.mdx",sourceDirName:".",slug:"/",permalink:"/",draft:!1,editUrl:"https://github.com/zenguardxyz/docs/tree/main/packages/create-docusaurus/templates/shared/docs/index.mdx",tags:[],version:"current",sidebarPosition:1,frontMatter:{sidebar_position:1},sidebar:"tutorialSidebar",next:{title:"Getting Started",permalink:"/category/getting-started"}},m={},h=[{value:"About:",id:"about",level:2}],k={toc:h},g="wrapper";function b(e){let{components:t,...a}=e;return(0,r.kt)(g,(0,n.Z)({},k,a,{components:t,mdxType:"MDXLayout"}),(0,r.kt)("h1",{id:"introduction"},"Introduction"),(0,r.kt)("blockquote",null,(0,r.kt)("p",{parentName:"blockquote"},(0,r.kt)("strong",{parentName:"p"},"Smarter Module Marketplace\nFor your Smart Accounts"))),(0,r.kt)("p",null,(0,r.kt)("strong",{parentName:"p"}," Extending your smart accounts is now as ",(0,r.kt)("em",{parentName:"strong"},"easy"),"\nand ",(0,r.kt)("em",{parentName:"strong"},"secure")," as installing an app on your smart phone ")),(0,r.kt)(d,{className:"green",onClick:()=>{window.open("https://safe2link.xyz","_blank")},mdxType:"Button"},"Module Demo"),"\xa0\xa0\xa0\xa0",(0,r.kt)(d,{className:"outline",onClick:()=>{window.open("https://app.safe.global/share/safe-app?appUrl=https://explore.zenguard.xyz&chain=sep","_blank")},mdxType:"Button"},"Try Marketplace"),(0,r.kt)("h2",{id:"about"},"About:"),(0,r.kt)("p",null,"ZenGuard stands as a pioneering app store dedicated to smart contract wallets. Here, developers can easily publish and monetize their cutting-edge features, while users seamlessly enhance their accounts with top-tier security measures and user-friendly upgrades. Each feature is meticulously crafted as a ",(0,r.kt)("a",{parentName:"p",href:"https://docs.safe.global/smart-account-modules"},"Safe modules"),", ensuring a secure and streamlined experience for all users involved."),(0,r.kt)("p",null,"At ZenGuard, we have designed a platform for publishing, exploring, and enabling these modules. Understanding that the security of these modules is of utmost importance, we will be onboarding module auditors through our module ",(0,r.kt)("a",{parentName:"p",href:"https://dashboard.zenguard.xyz"},"dashboard"),"."),(0,r.kt)("p",null,"ZenGuard provides all these with the help of platform that: "),(0,r.kt)("ol",null,(0,r.kt)("li",{parentName:"ol"},"Onboards developers to build and publish modules, streamlining the development process."),(0,r.kt)("li",{parentName:"ol"},"Onboards auditors to provide security assurance through onchain audit attestations for modules"),(0,r.kt)("li",{parentName:"ol"},"A module marketplace for Safe account users to exlpore, enable and upgrade the modules")),(0,r.kt)("blockquote",null,(0,r.kt)("p",{parentName:"blockquote"},"\u2139\ufe0f ",(0,r.kt)("strong",{parentName:"p"},"BACKGROUND: Safe Modules"),":")),(0,r.kt)("blockquote",null,(0,r.kt)("p",{parentName:"blockquote"}," Safe Modules enable Safe Accounts to add additional custom features in the form of standardized Smart Contracts. These smart contract logic can be anything from Social Recovery, Passkey Auth, Session Key to DeFi Automation, and much more. We have created a non-exhaustive list ",(0,r.kt)("a",{parentName:"p",href:"https://notes.zenguard.xyz/module-research"}," here "),".")),(0,r.kt)("p",null,(0,r.kt)("strong",{parentName:"p"}," A few of these modules can be: ")),(0,r.kt)("ul",null,(0,r.kt)("li",{parentName:"ul"},(0,r.kt)("strong",{parentName:"li"},"Session keys")," : Allow third part application to perform transaction on user's behalf with a session key with limited capability."),(0,r.kt)("li",{parentName:"ul"},(0,r.kt)("strong",{parentName:"li"},"Transaction limit")," :  Limit the transactions based on number of transactions/ daily spend limit etc."),(0,r.kt)("li",{parentName:"ul"},(0,r.kt)("strong",{parentName:"li"},"Auto payment")," : Allow account to make auto payments at a regular interval.")))}b.isMDXComponent=!0}}]);
