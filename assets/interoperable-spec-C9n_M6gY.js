e60840cda2f360a5baaecb1bb19e2f874f98baaf
Koshik Raj <koshik.raj@gmail.com>
2024-06-05 15:01:29 +0530

    Updates

---
+++ assets/interoperable-spec-C9n_M6gY.js
@@ -0,0 +1,21 @@
+import{u as s,j as e}from"./index-DNqROEbC.js";const i={title:"Interoperable standards",description:"undefined"};function r(n){const t={a:"a",div:"div",h1:"h1",h2:"h2",h3:"h3",header:"header",li:"li",p:"p",ul:"ul",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.header,{children:e.jsxs(t.h1,{id:"interoperable-standards",children:["Interoperable standards",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#interoperable-standards",children:e.jsx(t.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(t.h3,{id:"safe-modules-that-are-interoperable-with-other-smart-accounts",children:["Safe modules that are interoperable with other smart accounts.",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#safe-modules-that-are-interoperable-with-other-smart-accounts",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(t.p,{children:"These are modules developed using interoperable standards such as ERC-7579 and ERC-6900."}),`
+`,e.jsxs(t.h2,{id:"features",children:["Features:",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#features",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(t.p,{children:"There are numerous usecases and benefits of building the modules on these standards."}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"The standards are adapted by majority of smart account providers are such as Biconomy, ZeroDev, Alchemy and much more."}),`
+`,e.jsx(t.li,{children:"Wider support for tools and testing"}),`
+`,e.jsx(t.li,{children:"Supports Account Abstraction via ERC-4337 spec- This means, it supports the AA features such as transaction batching, fee sponsorship and much more."}),`
+`]}),`
+`,e.jsx(t.p,{children:"There are 2 prominent standards to develop the modules. Follow these guides to start buildin modules using them"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"/getting-started/erc-7579",children:"ERC-7579"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"/getting-started/erc-6900",children:"ERC-6900"})}),`
+`]}),`
+`,e.jsxs(t.h3,{id:"additional-resources",children:["Additional Resources",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#additional-resources",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://github.com/zenguardxyz/module-templates",children:"List of Module Templates"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://github.com/koshikraj/module-template-7579",children:"7579 Module template"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://erc7579.com",children:"ERC 7579"})}),`
+`]})]})}function d(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(r,{...n})}):r(n)}export{d as default,i as frontmatter};
