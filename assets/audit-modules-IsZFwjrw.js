b5243bcea1fb046eda43a9a4d84f483e6ffc159c
Koshik Raj <koshik.raj@gmail.com>
2024-06-27 21:04:37 +0530

    Updates

---
+++ assets/audit-modules-IsZFwjrw.js
@@ -0,0 +1,23 @@
+import{u as i,j as e}from"./index-3EaIJkvT.js";const n={title:"Add Audit attestations",description:"undefined"};function a(d){const t={a:"a",div:"div",h1:"h1",h2:"h2",header:"header",img:"img",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...i(),...d.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.header,{children:e.jsxs(t.h1,{id:"add-audit-attestations",children:["Add Audit attestations",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#add-audit-attestations",children:e.jsx(t.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsx(t.p,{children:"ZenGuard provides security assurance for its modules through on-chain audit attestations issued by auditors. Auditors submit these audit attestations via our dashboard, where they are embedded on-chain on EAS (Ethereum Attestation Service). These attestations are later used to verify the security of modules when a Safe user enables them on their Safe Account."}),`
+`,e.jsxs(t.h2,{id:"erc-7512-standard",children:["ERC-7512 Standard",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#erc-7512-standard",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(t.p,{children:["The audit attestation follows the ",e.jsx(t.a,{href:"https://github.com/ethereum/ercs/blob/master/ERCS/erc-7512.md",children:"ERC-7512"})," standard. For a detailed explanation of how ZenGuard leverages ERC-7512, visit our post on ",e.jsx(t.a,{href:"https://7512.zenguard.xyz",children:"7512.zenguard.xyz"}),"."]}),`
+`,e.jsxs(t.h2,{id:"steps-for-auditors-to-attest-modules",children:["Steps for Auditors to Attest Modules",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#steps-for-auditors-to-attest-modules",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Verify Account on Dashboard"}),": Auditors must verify their accounts on the ",e.jsx(t.a,{href:"https://dashboard.zenguard.xyz",children:"ZenGuard Dashboard"}),"."]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Perform Audits on Desired Module"}),": Find the relevant information about the module, including on-chain deployment details, codebase, and application specifics. Conduct audits according to the provided guidelines."]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Add Audit attestation"}),":  Once audits are completed, auditors can add audit details as attestations for the respective modules via the dashboard."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(t.h2,{id:"accessing-audit-details",children:["Accessing Audit Details",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#accessing-audit-details",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(t.p,{children:["Once a module is attested, audit details become accessible on the ZenGuard Module ",e.jsx(t.a,{href:"https://explore.zenguard.xyz",children:"Marketplace"}),"."]}),`
+`,e.jsxs(t.h2,{id:"module-audit-attestation-flow-demo",children:["Module Audit Attestation Flow Demo",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#module-audit-attestation-flow-demo",children:e.jsx(t.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.a,{href:"https://youtu.be/A7qqQ1nXi74",children:"A demo for the audit attestation verification for a 7579 based module for Safe Account"}),"."]}),`
+`]}),`
+`,e.jsx(t.p,{children:e.jsx(t.a,{href:"https://youtu.be/A7qqQ1nXi74",children:e.jsx(t.img,{src:"/img/zen-audit.gif",alt:"Audit Attestation Demo"})})})]})}function r(d={}){const{wrapper:t}={...i(),...d.components};return t?e.jsx(t,{...d,children:e.jsx(a,{...d})}):a(d)}export{r as default,n as frontmatter};
